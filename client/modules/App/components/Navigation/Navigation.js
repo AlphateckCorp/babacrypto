2f7ee75fdc78c5c408fd5263c94d3f078c9eb806
Varun Handa <webguru221@gmail.com>
2018-04-16 19:36:06 +0530

    close mobile menu on click on menu

--- client/modules/App/components/Navigation/Navigation.js
+++ client/modules/App/components/Navigation/Navigation.js
@@ -1,62 +1,51 @@
 
 import React, { Component } from 'react';
-import { Link } from 'react-router';
-import { slide as Menu } from 'react-burger-menu'
+import { Link, browserHistory } from 'react-router';
+import { slide as Menu } from 'react-burger-menu';
+import { WindowResizeListener } from 'react-window-resize-listener'
 
 class Navigation extends Component {
+    constructor(props) {
+        super(props)
+        this.state = { menuOpen: false }
+    }
+    closeMenu = (event) => {
+        // Do not call event.preventDefault() in here
+        this.setState({ menuOpen: false })
+    }
 
     render() {
+        const t =this;
         return (
-            // <nav className="navbar">
-            //     <div className="containerFluid">
-            //         <div className="navbarHeader">
-            //             <button type="button" className="navbarToggle" data-toggle="collapse" data-target="#myNavbar">
-            //                 <span className="navbarToggleIconBar"></span>
-            //                 <span className="navbarToggleIconBar"></span>
-            //                 <span className="navbarToggleIconBar"></span>
-            //             </button>
-            //             <Link className="navbarBrand" to="/">
-            //                 <span className="t--blue">baba</span><span>crypto</span><span className="">.com</span>
-            //             </Link>
-            //         </div>
-            //         <div className="collapse navbar-collapse" id="myNavbar">
-            //             <ul className="nav navbarNav">
-            //                 <li className="floatLeft navLi">
-            //                     <Link className="navbarLiA navLiA" to="/">COINS</Link>
-            //                 </li>                         
-            //                 <li className="floatLeft  navLi">
-            //                     <Link className="navbarLiA navLiA" to="/exchanges">Exchanges</Link>
-            //                 </li>
-            //             </ul>
-            //         </div>
-            //     </div>
-            // </nav>
-
             <div>
-        <nav id="menu">
-            <ul id="menu-closed">
-                <li>
-                    <Link className="navbrand"  to="/">
-                           <span className="t--blue">baba</span><span>crypto</span><span className="">.com</span>
-                    </Link>
-                
-                </li>
-                <li>
-                <Link className="" to="/">COINS</Link>
+                <WindowResizeListener onResize={windowSize => {
+                    if(windowSize.windowWidth > 768) {
+                        t.setState({ menuOpen: false })
+                    }
+                }}/>
+                <nav id="menu">
+                    <ul id="menu-closed">
+                        <li>
+                            <Link className="navbrand"  to="/">
+                                <span className="t--blue">baba</span><span>crypto</span><span className="">.com</span>
+                            </Link>                        
                         </li>
-                <li> <Link className="" to="/exchanges">Exchanges</Link></li>
-                <li><a href="#menu-closed">&#215; </a></li>
-                <li><a href="#menu">&#9776;</a></li>
-            </ul>
-        </nav>
-        <Link className="navbrand_logo"  to="/">
-                           <span className="t--blue">baba</span><span>crypto</span><span className="">.com</span>
-                    </Link>
-            <Menu right>
-                <Link id="coins" className="menu-item" to="/">COINS</Link>
-                <Link id="exchanges" className="menu-item" to="/exchanges">EXCHANGES</Link>
-            </Menu>
-        </div>
+                        <li>
+                            <Link className="" to="/">COINS</Link>
+                        </li>
+                        <li>
+                            <Link className="" to="/exchanges">Exchanges</Link>
+                        </li>
+                    </ul>
+                </nav>
+                <Link className="navbrand_logo"  to="/">
+                    <span className="t--blue">baba</span><span>crypto</span><span className="">.com</span>
+                </Link>
+                <Menu right isOpen={this.state.menuOpen}>
+                    <Link id="coins" className="menu-item" to="/" onClick={this.closeMenu}>COINS</Link>
+                    <Link id="exchanges" className="menu-item" to="/exchanges" onClick={this.closeMenu}>EXCHANGES</Link>
+                </Menu>
+            </div>
 
         );
     }
