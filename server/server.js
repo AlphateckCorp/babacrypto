07b2d8d48881638eb02e46ff23b35f46b8e037de
webguru221 <webguru221@gmail.com>
2018-07-21 12:23:53 +0530

    changed server path

--- server/server.js
+++ server/server.js
@@ -52,8 +52,8 @@ app.use(function(req, res, next) {
   next();
 });
 app.use('/api',  function(req, res) {  
-    var url = "http://babacrypto.local" + req.url;
-    // var url = "http://api.babacrypto.com" + req.url;
+    // var url = "http://babacrypto.local" + req.url;
+    var url = "http://api.babacrypto.com" + req.url;
 
     req.pipe(request(url)).pipe(res);
   });
