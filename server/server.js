21436782e247dc047bce255b8b685e6697039326
webguru221 <webguru221@gmail.com>
2018-10-15 19:40:55 +0530

    changed config paths

--- server/server.js
+++ server/server.js
@@ -63,6 +63,8 @@ app.use('/api', function (req, res) {
 });
 
 app.get('/*', function (req, res, next) {
+  console.log(req.headers.host, '...req.headers.host');
+  
   if (req.headers.host.match(/^www/) == null) res.redirect(301, 'https://www.babacrypto.com' + req.url);
   else next();
 });
